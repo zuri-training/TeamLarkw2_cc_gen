b6d17d143b08d0dec8a7764ff6705302327d3d4e
askdebb <53785246+askdebb@users.noreply.github.com>
2022-12-16 22:29:44 +0000

    busy

--- cc_gen/helpers/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class CreditCardDesign(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        abstract = True
-        ordering = ('-created_at',)
