7ebeaa5ead54f12f2de0411c8f50e2c8166792bd
askdebb <53785246+askdebb@users.noreply.github.com>
2022-12-21 12:04:43 +0000

    uploaded files for deployment

--- cc_gen/cc_gen_App/urls.py
+++ cc_gen/cc_gen_App/urls.py
@@ -1,6 +1,7 @@
 import imp
 from django.urls import path
 from . import views
+from django.http import HttpResponse
 
 urlpatterns =[
     path('',views.home, name="homePage"), 
--- cc_gen/cc_gen_App/views.py
+++ cc_gen/cc_gen_App/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from django.utils import 
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'index.html',{})
+    return render(request,'index.html')
 
 def product(request):
-    return render(request,'product.html',{})
+    return render(request,'product.html')
 
 def contact(request):
-    return render(request,'contact.html',{})
-
-
+    return render(request,'contact.html')
 
 def faq(request):
-    return render(request,'faq.html',{})
-
+    return render(request,'faq.html')
 
 
 def aboutus(request):
-    return render(request,'about-us.html',{})
+    return render(request,'about-us.html')
 
 
-def contactPage(request):
-    if request.method == 'post':
-        pass
-    else:   
-        return render(request,'contact.html',{})
+# def contactPage(request):
+#     if request.method == 'post':
+#         pass
+#     else:   
+#         return render(request,'contact.html',{})
